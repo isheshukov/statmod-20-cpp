34bfb908264de49d54b988286cd428da2fdd1e8e
Ilia Sheshukov <sheshukov.ilya@gmail.com>
2020-10-06 23:58:10 +0300

    added qcustomplot dependency

--- app/mainwindow.cpp
+++ app/mainwindow.cpp
@@ -1,19 +1,20 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget* parent)
+  : QMainWindow(parent)
+  , ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+  ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+  delete ui;
 }
 
-void MainWindow::on_actionExit_triggered()
+void
+MainWindow::on_actionExit_triggered()
 {
-    QApplication::quit();
+  QApplication::quit();
 }
